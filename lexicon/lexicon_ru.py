cb4d17f9337e2f91c58b097459e5b9021d7e4256
A0Grafsky <145706286+A0Grafsky@users.noreply.github.com>
2024-07-30 12:07:19 +0300

    Полностью готовый бот!

--- lexicon/lexicon_ru.py
+++ lexicon/lexicon_ru.py
@@ -3,4 +3,5 @@ LEXICON_RU: dict[str, str] = {
     'remove_first': 'Удаление первых n-человек',
     'remove_last': 'Удаление последних n-человек',
     'remove_deleted': 'Удаление неактивных пользователей',
+    'print_all_user': 'Показать всех пользователей'
 }
